dd5a4f08c0c280c031772ba4d53f30d5b314bda2
Matt Zidell <mzidell@ucsd.edu>
2025-10-14 22:51:50 -0700

    JS exercise

---
+++ docs/global.js
@@ -0,0 +1,37 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// navLinks = $$("nav a") 
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// currentLink.classList?.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'portfolio/', title: 'Portfolio' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = `/docs/${p.url}`;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/' // Local server
+    : '/website/'; // GitHub Pages repo name
+
+if (!url.startsWith('http')) {
+  url = BASE_PATH + url;
+}
